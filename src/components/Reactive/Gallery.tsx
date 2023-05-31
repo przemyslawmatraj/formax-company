ab5aee1d0969fe0ec1156de54af125603922f61b
przemyslawmatraj <Przemyslaw.Matraj@microsoft.wsei.edu.pl>
2023-05-31 20:53:50 +0200

    feat: add Baner, Mission and Portfolio components

---
+++ src/components/Reactive/Gallery.tsx
@@ -0,0 +1,50 @@
+import { useState } from 'react';
+
+import LightGallery from 'lightgallery/react';
+
+// import styles
+import 'lightgallery/css/lightgallery.css';
+import 'lightgallery/css/lg-zoom.css';
+import 'lightgallery/css/lg-thumbnail.css';
+
+// import plugins if you need
+import lgThumbnail from 'lightgallery/plugins/thumbnail';
+import lgZoom from 'lightgallery/plugins/zoom';
+
+export interface Image {
+    src: string;
+    thumb: string;
+}
+
+function Gallery({images, title}: {images: Image[], title: string}) {
+    const [isMore, setIsMore] = useState(false);
+
+    return <>
+            <div className='mx-auto container my-[var(--space-l-2xl)]' id="main-gallery">
+            <h3 className="h3 my-[var(--space-l-2xl)] capitalize">{title}</h3>
+                <LightGallery
+                speed={500}
+                plugins={[lgThumbnail, lgZoom]}
+                mode="lg-fade"
+                download={false}
+                zoom={false}
+            >
+                {images.slice(
+            0,
+            isMore ? images.length : 6
+        ).map((image) => (
+                   <a href={image.src} key={image.src} className="inline-block m-1 p-1 w-[47%] lg:w-[24%]" style={{display: "inline-block", margin: "5px"}}>
+                   <img alt="img2" width="100%" src={image.thumb} />
+                   <p className="text-gray-500 mt-2">{title}</p>
+               </a>
+                ))}
+            </LightGallery>
+            </div>
+                <button onClick={() => setIsMore(prev => !prev)} className="uppercase font-semibold text-red-600 mt-14 block mx-auto border-2 border-red-600 p-4">Zobacz {
+                    isMore ? "mniej" : "więcej"
+                }</button>
+
+                </>;
+}
+
+export default Gallery;
---
+++ src/components/Reactive/Services.tsx
@@ -0,0 +1,28 @@
+
+import { useState } from 'react';
+import services from'@config/services.json';
+
+const Services = () => {
+    const [isMore, setIsMore] = useState(false);
+
+    return <section className="container mx-auto mt-[var(--space-2xl-3xl)]">
+    <h2 className="h2 text-center capitalize">Nasza oferta</h2>
+    <p className="text-gray-500 text-center mt-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+    <div className="grid grid-cols-[repeat(auto-fill,minmax(300px,1fr))] lg:grid-cols-3 gap-4 mt-6">
+        {services.slice(
+            0,
+            isMore ? services.length : 6
+        ).map((service) => (
+            <article className="bg-white rounded-lg p-4" key={service.name}>
+                <img src={ service.image } alt={ service.name } className="w-full h-48 object-cover object-center rounded-lg" />
+                <h3 className="h3 mt-4">{ service.name }</h3>
+                <p className="text-gray-500 mt-2">{ service.description}</p>
+                <a href={ service.url } className="uppercase font-semibold text-red-600 mt-5 block">Zobacz</a>
+            </article>
+        ))}
+    </div>
+    <button onClick={() => setIsMore(!isMore)} className="uppercase font-semibold text-red-600 mt-14 block mx-auto border-2 border-red-600 p-4">Zobacz {isMore ? "mniej" : "więcej"}</button>
+</section>
+}
+
+export default Services;
